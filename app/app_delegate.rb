58d4023bf93201987f7a1cc6a385ccbb987bd20a
tiffaniechia <tiffaniekyrachia@gmail.com>
2015-08-04 17:12:27 +0800

    exploring uitabbarcontroller

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,11 +1,27 @@
 class AppDelegate
 
+  # ios sdk uses uinavigationcontroller and uitabbarcontroller for continaing child view ontrollers
+  # uinavigationcontroller and uitabbarcontroller are uiviewcontroller subclasses which actually manager other uiviewcontrollers
+  # they have a view just like regular controllers to which children controller's views are added as subviews
+
   def application(application, didFinishLaunchingWithOptions: launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.makeKeyAndVisible
 
     controller = TapController.alloc.initWithNibName(nil, bundle: nil)
-    @window.rootViewController = UINavigationController.alloc.initWithRootViewController(controller)
+    nav_controller = UINavigationController.alloc.initWithRootViewController(controller)
+
+    other_controller = UIViewController.alloc.initWithNibName(nil, bundle: nil)
+    other_controller.title = 'other'
+    other_controller.view.backgroundColor = UIColor.purpleColor
+
+    # unlike other containers uitabbarcontrollers are only supposed to act as the rootviewcontroller of the window
+    # which means you shouldnt push a tab bat controller inside a navigation controller
+    tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
+    # uses viewcontrollers to control its children
+    tab_controller.viewControllers = [nav_controller, other_controller]
+
+    @window.rootViewController = tab_controller
 
     true
   end
--- app/controllers/TapController.rb
+++ app/controllers/TapController.rb
@@ -17,12 +17,26 @@ class TapController < UIViewController
 
     @right_button = UIBarButtonItem.alloc.initWithTitle('Push', style: UIBarButtonItemStyleBordered, target: self, action: 'push')
 
+    # navigation controller uses navigation item to change buttons and other options of the top blue bar
     self.navigationItem.rightBarButtonItem = @right_button
   end
 
   def push
     @new_controller = TapController.alloc.initWithNibName(nil, bundle: nil)
+    # navigation controller uses pushviewcontroller:animated and popviewcontrolleranimated to control the stack
+    # by default the navigationcontroller will also show a back button which handles the popping of the current controller for s
+    # normally we call popviewcontrolleranimated
     self.navigationController.pushViewController(@new_controller, animated: true)
+  end
+
+  # bundle because we want it to create the tabbaritem as soon as the controller exists regardless of whether or not its view exits
+  # so if you put it in viewdidload then it might not get created whent he app launches
+  # initwithtitle:image:tag: if you want to supply a custom image and a title.
+  # image as to be 30x30 black and transparent icon
+  def initWithNibName(name, bundle: bundle)
+    super
+    self.tabBarItem = UITabBarItem.alloc.initWithTabBarSystemItem(UITabBarSystemItemFavorites, tag: 1)
+    self
   end
 
 end
