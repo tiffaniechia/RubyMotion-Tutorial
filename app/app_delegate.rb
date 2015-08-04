ba188e9f395e4a717f8b4aa001cb42db84f07553
tiffaniechia <tiffaniekyrachia@gmail.com>
2015-08-04 14:27:04 +0800

    exploring uiviews on rubymotion

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,19 +1,24 @@
 class AppDelegate
-  def application(application, didFinishLaunchingWithOptions:launchOptions)
-
-    alert = UIAlertView.new
-    alert.message = 'Hello World!'
-    alert.show
-
-    rootViewController = UIViewController.alloc.init
-    rootViewController.title = 'Hello'
-    rootViewController.view.backgroundColor = UIColor.whiteColor
-
-    navigationController = UINavigationController.alloc.initWithRootViewController(rootViewController)
+  def application(application, didFinishLaunchingWithOptions: launchOptions)
 
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    @window.rootViewController = navigationController
     @window.makeKeyAndVisible
+
+    @blue_view = UIView.alloc.initWithFrame(CGRectMake(10,10,100,100))
+    @blue_view.backgroundColor = UIColor.blueColor
+
+    @window.addSubview(@blue_view)
+
+    @green_view = UIView.alloc.initWithFrame(CGRectMake(30,30,40,40))
+    @green_view.backgroundColor = UIColor.greenColor
+
+    @window.addSubview(@green_view)
+
+    @red_view = UIView.alloc.initWithFrame(CGRectMake(30,30,50,50))
+    @red_view.backgroundColor = UIColor.redColor
+
+    @blue_view.addSubview(@red_view)
+
 
     true
   end
