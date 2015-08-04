c346caf5f6460d525ba9667fbb3f89451434a9f8
tiffaniechia <tiffaniekyrachia@gmail.com>
2015-08-04 16:08:03 +0800

    exploring uinavigationcontrollers

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,18 +1,13 @@
 class AppDelegate
-  def application(application, didFinishLaunchingWithOptions: launchOptions)
 
+  def application(application, didFinishLaunchingWithOptions: launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.makeKeyAndVisible
 
-    # window will take the given uiviewcontroller and adjust its view's size to fit the window
-    # better way of setting up your window as opposed to window.addsubview errwhere
-
-    # initwithnibname. to load controller from NIB file (created using xcode interface builder to visually construct view)
-    # but no xcode so we can safely pass nil
-    # it is also the 'designated initializer ' of uiciewcontrollers.
-    # hence whenever you want to create a controller you must call this method at some point
-    @window.rootViewController = TapController.alloc.initWithNibName(nil, bundle: nil)
+    controller = TapController.alloc.initWithNibName(nil, bundle: nil)
+    @window.rootViewController = UINavigationController.alloc.initWithRootViewController(controller)
 
     true
   end
+
 end
--- app/controllers/TapController.rb
+++ app/controllers/TapController.rb
@@ -1,20 +1,28 @@
 class TapController < UIViewController
-  # runs after self.view is loaded
-  def viewDidLoad
 
-    # must call super in uiviewcontroller
+  def viewDidLoad
     super
 
     self.view.backgroundColor = UIColor.whiteColor
 
-    # rectzero because we don't know the exact starting size
     @label = UILabel.alloc.initWithFrame(CGRectZero)
     @label.text = 'Taps'
 
-    # size to fit and center to center the controller's view
     @label.sizeToFit
     @label.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2)
 
     self.view.addSubview @label
+
+    self.title = "Tap (#{self.navigationController.viewControllers.count})"
+
+    @right_button = UIBarButtonItem.alloc.initWithTitle('Push', style: UIBarButtonItemStyleBordered, target: self, action: 'push')
+
+    self.navigationItem.rightBarButtonItem = @right_button
   end
+
+  def push
+    @new_controller = TapController.alloc.initWithNibName(nil, bundle: nil)
+    self.navigationController.pushViewController(@new_controller, animated: true)
+  end
+
 end
